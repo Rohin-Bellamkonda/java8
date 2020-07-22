29f4df339ec8d7f08787bc5b947616b9c0b184d2
Rohin-Bellamkonda <17pa1a1205@vishnu.edu.in>
2020-07-22 21:28:47 +0530

    commit

---
+++ java8/src/main/java/Epam/Login/Average.java
@@ -0,0 +1,10 @@
+package Epam.Login;
+
+
+import java.util.*;
+public class Average {
+	public double avg(List<Integer> list)
+	{
+		return list.stream().mapToInt(number -> number.intValue()).average().getAsDouble();
+	}
+}
---
+++ java8/src/main/java/Epam/Login/StringManipulate.java
@@ -0,0 +1,12 @@
+package Epam.Login;
+
+
+import java.util.*;
+import java.util.stream.Collectors;
+public class StringManipulate {
+	public List<String> stringManipulate(List<String> list)
+	{
+		return list.stream().filter(s -> s.startsWith("a")).filter(s -> s.length() == 3).collect(Collectors.toList());
+	}
+}
+
